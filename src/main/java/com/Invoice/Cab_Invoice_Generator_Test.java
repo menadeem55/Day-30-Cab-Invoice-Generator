2b7ee9142bb452d8ecac33841f870c7dffb1fc29
me.nadeem55@gmail.com <me.nadeem55@gmail.com>
2022-11-22 22:04:16 +0530

    [Added: Calculate fare of Ride]

--- src/main/java/com/Invoice/Cab_Invoice_Generator_Test.java
+++ src/main/java/com/Invoice/Cab_Invoice_Generator_Test.java
@@ -5,20 +5,18 @@ import org.junit.Test;
 
 public class Cab_Invoice_Generator_Test {
 	@Test
-	public void CalculateFareTest() {
-		Cab_Invoice_Generator obj = new Cab_Invoice_Generator();
-		double test = obj.calculateFare(10, 15);
-		double test1 = obj.calculateFare(10, 15) + obj.calculateFare(10, 15);
-		;
+	public void CalculateFareTest()  {
+		Cab_Invoice_Generator obj=new Cab_Invoice_Generator();
+		double test=obj.calculateFare(10, 15);
+		double test1=obj.calculateFare(10, 15)+obj.calculateFare(10, 15);;
 		Assert.assertEquals(230.0, test1, 0.0);
 	}
-
 	@Test
-	public void MonthFareTest() {
-		Cab_Invoice_Generator obj = new Cab_Invoice_Generator();
-		Ride[] rides = { new Ride(1, 23), new Ride(1, 2) };
-		double monthfare = obj.calculateFare(rides);
-		Assert.assertEquals(45.0, monthfare, 0.0);
-
-	}
+	public void MonthFareTest()  {
+		Cab_Invoice_Generator obj=new Cab_Invoice_Generator();
+		Ride[] rides= { new Ride(1,23), new Ride(1,2)};
+		double monthfare=obj.calculateFare(rides);
+		Assert.assertEquals(45.0,monthfare , 0.0);
+		
+		}
 }
